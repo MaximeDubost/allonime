21f6ddd824758078068a502660e3b394e26725c1
Aurelien <aureleinguillemot@hotmail.fr>
2020-06-17 10:15:04 +0200

    nav bar & onglet

--- allonime/src/app/app-routing.module.ts
+++ allonime/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
+import { DashboardComponent }   from './dashboard/dashboard.component';
+import { OpeningComponent }     from './opening/opening.component';
+import { EndingComponent }     from './ending/ending.component';
+import { GifComponent }     from './gif/gif.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'opening', component: OpeningComponent },
+  { path: 'ending', component: EndingComponent },
+  { path: 'gif', component: GifComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- allonime/src/app/app.module.ts
+++ allonime/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AnimeComponent } from './anime/anime.component';
+import { OpeningComponent } from './opening/opening.component';
+import { EndingComponent } from './ending/ending.component';
+import { GifComponent } from './gif/gif.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NavbarComponent,
+    AnimeComponent,
+    OpeningComponent,
+    EndingComponent,
+    GifComponent
   ],
   imports: [
     BrowserModule,
