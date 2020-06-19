078edce948c802e722bb8d9812664dee53b9612a
Kévin <kevtsiii@gmail.com>
2020-06-19 10:49:01 +0200

    effect

--- allonime/src/app/anime-detail/videoLauncher.js
+++ allonime/src/app/anime-detail/videoLauncher.js
@@ -4,10 +4,14 @@ videoLauncher.launch = () => {
         $(".opening").click(() => {
             $("#anime-openings").show();
             $("#anime-endings").hide();
+            $(".ending").removeClass("active")
+            $(".opening").addClass("active")
         })
         $(".ending").click(() => {
             $("#anime-openings").hide();
             $("#anime-endings").show();
+            $(".ending").addClass("active")
+            $(".opening").removeClass("active")
         })
     });
 }
